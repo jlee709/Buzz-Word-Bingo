b40882e38dafca40f61490b0e87f1314b9ef0fe5
Justin Lee <leejwg.3@gmail.com>
2017-10-06 19:54:25 -1000

    currently get and post work. recieves POST , responds with JSON object, Sends back JSON Object on Res

--- app.js
+++ app.js
@@ -7,8 +7,12 @@ let app = express();
 let PORT = process.env.PORT || 3000;
 var bodyParser = require('body-parser');
 
+//the-buzz-words array variable
+var buzzArry =[];
+
 //uses public dir in express invokation
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:false}));
 
 //routes
 
@@ -16,19 +20,23 @@ app.get('/', (req, res)=> {
   res.send('Hello');
 });
 
-app.get('/buzzwords', (req, res) => {
-  res.send('this is the buzzwords');
+app.get('/the-buzz-words', (req, res) => {
+  res.json({"buzzwords": buzzArry});
 });
 
-app.post('/buzzwords', (req, res) =>{
-  console.log('will do post on buzzwords');
+app.post('/the-buzz-words', (req, res) =>{
+  console.log('will do post on the-buzz-words');
+   buzzArry.push(req.body);
+   console.log(req.body);
+   res.json({ "success": true });
 });
 
-app.put('/buzzwords', (req, res) => {
+app.put('/the-buzz-words', (req, res) => {
   console.log('does put reuest');
+
 });
 
-app.delete('/buzzwords', (req, res) => {
+app.delete('/the-buzz-words', (req, res) => {
   console.log('adds delete method');
 });
 
@@ -39,6 +47,8 @@ app.post('/reset', (req, res) => {
   
 
 
+
+  
 
 
 //port listens
