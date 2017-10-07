c5c97c254aa1fc9ba3e60d543e7110560df46a27
Justin Lee <leejwg.3@gmail.com>
2017-10-06 21:56:14 -1000

    put and post method is working, POST sending  PUt sending server Recponding with Correct JSON object and updates

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var bodyParser = require('body-parser');
 
 //the-buzz-words array variable
 var buzzArry =[];
-
+var score = 0;
 //uses public dir in express invokation
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended:false}));
@@ -24,19 +24,37 @@ app.get('/the-buzz-words', (req, res) => {
   res.json({"buzzwords": buzzArry});
 });
 
-app.post('/the-buzz-words', (req, res) =>{
+app.post('/the-buzz-word', (req, res) =>{
   console.log('will do post on the-buzz-words');
-   buzzArry.push(req.body);
-   console.log(req.body);
-   res.json({ "success": true });
+  for(let i = 0; i < buzzArry.length; i++){
+    if(buzzArry[i].buzzword === req.body.buzzword){
+      console.log('cant share the same buzzword');
+      return - 1;
+    }
+  }
+  buzzArry.push(req.body);
+  console.log(req.body);
+  res.json({ "success": true });
 });
 
-app.put('/the-buzz-words', (req, res) => {
-  console.log('does put reuest');
+app.put('/the-buzz-word', (req, res) => {
+  console.log('does put reqest');
+  for(let i = 0; i < buzzArry.length; i++){
 
+    if(buzzArry[i].buzzword === req.body.buzzword){
+      console.log('cant share the same buzzword');
+      buzzArry.push(req.body);
+      score = score + 1;
+      return res.json({ "success": true, newScore: score });
+
+    } 
+  }   
+
+  console.log('adding PUT');
+  return res.json({ "success": false, newScore: score  });
 });
 
-app.delete('/the-buzz-words', (req, res) => {
+app.delete('/the-buzz-word', (req, res) => {
   console.log('adds delete method');
 });
 
@@ -48,7 +66,7 @@ app.post('/reset', (req, res) => {
 
 
 
-  
+
 
 
 //port listens
