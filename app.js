8295e23c9a53e52d7f18df73c148dc67b3098426
Justin Lee <leejwg.3@gmail.com>
2017-10-06 16:10:04 -1000

    added json locn an unlock

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
+//jshint esversion:6
 console.log('iamhere');
+
+//initial depencies
+let express = require('express');
+let app = express();
+let PORT = process.env.PORT || 3000;
+
+
+//uses public dir in express invokation
+app.use(express.static('public'));
+
+
+
+
+
+
+
+
+
+//port listens
+app.listen(PORT, (err) => {
+  console.log('server ' + PORT);
+});
